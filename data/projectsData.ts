afff5b20592020e3fc1ac7b7f53a79cc02a8ebbc
Sergey S <sergesf@gmail.com>
2025-12-23 20:43:00 -0800

    some minir first customizations

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -6,20 +6,18 @@ interface Project {
 }
 
 const projectsData: Project[] = [
+  // {
+  //   title: 'A Search Engine',
+  //   description: `What if you could look up any information in the world? Webpages, images, videos
+  //   and more. Google has many features to help you find exactly what you're looking
+  //   for.`,
+  //   imgSrc: '/static/images/google.png',
+  //   href: 'https://www.google.com',
+  // },
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
+    title: 'WordCraft Arena Game',
+    description: `Turn-by-turn word game where players compete to create words with longest length. Front end built with Godot Engine and backend with NestJS distributed services.`,
+    imgSrc: '/static/images/wordcraft-store.png',
     href: '/blog/the-time-machine',
   },
 ]
